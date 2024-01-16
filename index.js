617e0e4c19190919498a5cffd5de028a2c289ea3
Prathmeshranpise <prathmeshranpisea07@gmail.com>
2024-01-16 13:42:13 +0530

    2 commit

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello bagale');
+ res.send('hello Prathmesh Ranpise');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
 module.exports = app;
+
+
